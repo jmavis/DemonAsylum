a824eb5ae4bc85e2f4801d344b99e94c80913fc4
jmavis <jaredmavis@outlook.com>
2014-08-14 20:38:07 -0700

    Porting over input basics

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+/*
+ *  InputManager.cs
+ * 		Will detect what type of input we should use and will continuously run it.
+ * 
+ */
+
+public class InputManager : MonoBehaviour {
+	#region Fields
+	private InputType typeOfInput;
+	#endregion
+	
+	void Start () {
+		typeOfInput = DetermineTypeOfInput();
+	}
+	
+	private InputType DetermineTypeOfInput() {
+		// Check what type of input we should be expecting
+		// if we are running on android or iOS then use touch controls else
+		// it is assumed this game will be ran on mobile or computers and thus other device inputs are not accounted for
+		if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer){
+			//TODOreturn (new TouchInput());
+			return (new MouseInput());
+		} else {
+			return (new MouseInput());
+		}
+	}
+	
+	void Update () {
+		typeOfInput.HandleInput();       
+	}
+}
--- Assets/Scripts/Input/InputTypes/InputType.cs
+++ Assets/Scripts/Input/InputTypes/InputType.cs
@@ -1,21 +1,34 @@
 using UnityEngine;
 using System.Collections;
 
-public abstract class InputType {
-	protected emum ControlState {
-		WaitingForFirstInput, // No fingers down
-		WaitingForSecondTouch, // One finger down waiting for seconds finger or movement 
-		WaitingForMovement, // Two fingers down and waiting for zoom indication
-		Draging, // User is dragging
-		Pinching, // User is pinching in or out
-		WaitingForNoInput, // Event is done and waiting for the user to stop input
-		HoldingClick, // User has held the click down for HOLDMINSECONDS
+/*
+ * InputType.cs
+ * Base class for input into game. These functions will be called at the appropriat time be a derived class.
+ */
+
+
+public abstract class InputType  {
+	// The input detection is based off of these states
+	protected enum ControlState {
+		WaitingForFirstInput, // There are no fingers down/ mouse down
+		WaitingForSecondTouch, // There is one finger down and we are waiting to see if user presses down another for 2 touch actions *note this will only occur on mobile
+		WaitingForMovement, // The user has pressed 2 fingers down and we are waiting to see what gesture is performed
+		DragingCamera, // The user has one finger down/clicked and is moving across the screen greater than some minimum amount
+		ZoomingCamera, // The state that occurs when the user move two fingers in opposite directions to zoom in and out or if using mouse when using the wheel
+		WaitingForNoInput, // The final state where the user's input has been performed but the user is still touching the screen/ clicking
+		HoldingClick // Where the user has help down a click and is continuing to hold
 	};
 	
-	protected float HOLDMINSECONDS = 2;
+	// General input system settings to be altered as seen fit
+	protected float minimumTimeUntilMove = .25f; // the time in seconds that we will wait for the user to move before we interprate as a tap
+	protected float minimumMovementDistance = 5; // the amount of posisiton change in a single touch gesture/click before it is considered a drag
+	protected bool zoomEnabled = true; 
+	protected float zoomEpsilon = 10;
+	protected float firstTouchTime;
 	
-	protected bool PINCH_IN = true;
-	protected bool PINCH_OUT = false;
+	// some variables to represent zooming in/out
+	protected bool ZOOM_IN  = true;
+	protected bool ZOOM_OUT = false;
 	
 	protected ControlState currentState;
 	
@@ -29,20 +42,44 @@ public abstract class InputType {
 	public abstract void ResetControlState();
 	#endregion
 	
+	#region InputEvents
+	// ------------ These functions will be called when the given event occurs, put any code to be perform on the event in here 
+	protected virtual void DragEvent(Vector2 inputChangeSinceLastTick){
+		//EventManager.instance.RiseOnDragEvent(new DragArgs(inputChangeSinceLastTick));
+	}
+	
+	// called when a click/tap occurs
+	protected void SingleClickEvent(Vector2 inputScreenPos){
+		//if (IsMenu() || !GUIManager.Instance.ClickOnGUI(inputScreenPos)){
+		//	DelegateClickForObjects(inputScreenPos);
+		//}
+	}
+	
+	// Tell the camera to zoom in or out
+	protected void ZoomEvent(bool isZoomingIn){
+		
+	}	
+	
+	protected void OnHoldClick(Vector2 inputScreenPos){
+		//if (IsMenu() || !GUIManager.Instance.ClickOnGUI(inputScreenPos)){
+		//	EventManager.instance.RiseOnClickHoldEvent(new ClickPositionArgs(inputScreenPos));
+		//}
+	}
+	
+	protected void OnHoldRelease(){
+		//EventManager.instance.RiseOnClickHoldReleaseEvent();
+	}
+	#endregion	
+	
 	#region Notifications
 	protected void NotifyNoObjectClickedOn(Vector2 inputScreenPos){
-		EventManager.instance.RiseOnClickedNoObjectEvent(new ClickPositionArgs(inputScreenPos));
+		//EventManager.instance.RiseOnClickedNoObjectEvent(new ClickPositionArgs(inputScreenPos));
 	}
 
 	protected void NotifyObjectClickedOn(GameObject objectClicked){
-		EventManager.instance.RiseOnClickedObjectEvent(new ClickedObjectArgs(objectClicked));
+		//EventManager.instance.RiseOnClickedObjectEvent(new ClickedObjectArgs(objectClicked));
 	}
 	#endregion
-	
-	protected void SingleClickEvent(Vector2 inputScreenPos){
-		// TODO check if on a menu
-		DelegateClickForObjects(inputScreenPos);	
-	}
 	
 	private void DelegateClickForObjects(Vector2 inputScreenPos) {
 		Ray ray = Camera.main.ScreenPointToRay (inputScreenPos);
@@ -56,13 +93,18 @@ public abstract class InputType {
 		}
 	}
 	
-	// Use this for initialization
-	void Start () {
-	
+	// will detect if the change in input position since the last tick is enough to be accepted as a drag
+	protected bool DragMovementDetected(Vector2 movementChange){
+		// if the x or y is greater than the minimum amount to be considered a drag then return true
+		if (Mathf.Abs(movementChange.x) > minimumMovementDistance || Mathf.Abs(movementChange.y) > minimumMovementDistance){
+			return (true);
+		} else {
+			return (false);
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	private bool IsMenu(){
+		return false;
+		//return (Application.loadedLevelName.Equals(Strings.TitleMenu) || Application.loadedLevelName.Equals(Strings.EndScreen));
 	}
 }
