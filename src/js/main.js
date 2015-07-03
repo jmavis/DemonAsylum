1735b3da371148bc25e1ac0e77687cb0cafb538b
jmavis <jaredmavis@outlook.com>
2015-07-02 22:42:46 -0700

    Make screen landscape

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(512, 1024, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(1024, 512, Phaser.AUTO, 'gameDiv');
 
 game.state.add(States.startup, startupState);
 game.state.add(States.game, gameState);
